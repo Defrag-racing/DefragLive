1c2a346083c882408929e371a89ebdd41f6035d8
Abdullah Hejazi <a.m.a.hejazi@gmail.com>
2022-12-02 14:39:42 +0200

    fixed getplayerbyid none bug

--- src/serverstate.py
+++ src/serverstate.py
@@ -281,9 +281,9 @@ def validate_state():
     global PAUSE_STATE
     global IGNORE_IPS
 
-    logging.info("[Debug] getplayerbyid: " + str(STATE.get_player_by_id(STATE.bot_id)))
     # Current player spectated is our bot, and thus idle.
-    spectating_self = STATE.curr_dfn == STATE.get_player_by_id(STATE.bot_id).dfn \
+    if STATE.get_player_by_id(STATE.bot_id) is not None:
+        spectating_self = STATE.curr_dfn == STATE.get_player_by_id(STATE.bot_id).dfn \
                       or STATE.current_player_id == STATE.bot_id
 
     # Current player spectated has turned on the no-spec system
