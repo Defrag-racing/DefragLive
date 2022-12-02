7f3f34f76d503d21c784952a48431abd39b84fee
Abdullah Hejazi <a.m.a.hejazi@gmail.com>
2022-12-02 17:52:52 +0200

    logging

--- src/api.py
+++ src/api.py
@@ -45,6 +45,7 @@ def api_init():
 
 
 def exec_command(cmd, verbose=True):
+    logging.info(f"Executing command {cmd}")
     if verbose:
         logging.info(f"Execing command {cmd}")
     # send the text to the console window, escape commas (must be `, to show up in chat)
--- src/serverstate.py
+++ src/serverstate.py
@@ -232,10 +232,6 @@ def initialize_state():
         secret = ''.join(random.choice('0123456789ABCDEF') for i in range(16))
         server_info, bot_player, timeout_flag = None, [], 0
 
-        new_ip = servers.get_next_active_server(IGNORE_IPS)
-        logging.info("[SERVERSTATE] Initial Connection Connecting Now : " + str(new_ip))
-        connect(new_ip)
-
         init_counter = 0
         while server_info is None or bot_player == []:  # Continue running this block until valid data and bot id found
             init_counter += 1
@@ -396,6 +392,7 @@ def connect(ip, caller=None):
     if caller is not None:
         STATE.connect_msg = f"^7Brought by ^3{caller}"
         IGNORE_IPS = []
+    logging.info("[SERVERSTATE] CONNECTING EXEC")
     api.exec_command("connect " + ip, verbose=False)
 
 
