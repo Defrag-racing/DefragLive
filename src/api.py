1a907ca4e3bcf3b356a108df4c3951f8ab2ca321
Abdullah Hejazi <a.m.a.hejazi@gmail.com>
2022-12-02 17:00:37 +0200

    switch serverstate thread with con_thread

--- src/api.py
+++ src/api.py
@@ -40,6 +40,8 @@ def api_init():
                     "\nFileAppend, %console%, * ;", blocking=True)
     WINDOW = AHK.find_window(process=config.DF_EXE_PATH, title=b"TwitchBot Engine")
 
+    logging.info("[DEBUG]" + str(CONSOLE is None) + " " + str(WINDOW is None))
+
     if CONSOLE is None or WINDOW is None:
         raise WindowNotFoundError
 
