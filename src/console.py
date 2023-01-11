fce880cdf4fcb230f2d820c5f40d7f8adae49fbe
Abdullah Hejazi <a.m.a.hejazi@gmail.com>
2023-01-11 19:16:42 +0200

    fix debug

--- src/console.py
+++ src/console.py
@@ -190,10 +190,11 @@ def process_line(line):
                 serverstate.STATE.init_vote()
                 api.exec_command("say ^7Vote detected. Should I vote yes or no? Send ^3?^7f1 for yes and ^3?^7f2 for no.")
 
-        if line.startswith('Not recording a demo.') or line.startswith("report written to system/reports/initialstate.txt"):
+        if line.startswith('Not recording a demo.'):
             print('HERE')
             print(serverstate.nospec_ids)
 
+        if line.startswith('Not recording a demo.') or line.startswith("report written to system/reports/initialstate.txt"):
             if serverstate.CONNECTING:
                 time.sleep(1)
                 serverstate.CONNECTING = False
