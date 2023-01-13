f82b5e9fdb52d324a0496deba46f7a5505d09665
Abdullah Hejazi <a.m.a.hejazi@gmail.com>
2023-01-13 21:23:10 +0200

    stopped demo recording

--- src/console.py
+++ src/console.py
@@ -190,6 +190,10 @@ def process_line(line):
                 serverstate.STATE.init_vote()
                 api.exec_command("say ^7Vote detected. Should I vote yes or no? Send ^3?^7f1 for yes and ^3?^7f2 for no.")
 
+
+        if line.startswith('Stopped demo recording.'):
+            print('HEEEEEEEEEEEERE')
+
         if line.startswith('Not recording a demo.') or line.startswith("report written to system/reports/initialstate.txt"):
             if serverstate.CONNECTING:
                 time.sleep(1)
