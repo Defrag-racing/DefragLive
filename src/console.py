d86bc652cb479069b76543ce4c4f0d39b253cb5d
neyoneit <neyoneit@gmail.com>
2023-04-20 18:00:40 +0200

    Another error thingy

--- src/console.py
+++ src/console.py
@@ -31,6 +31,7 @@ ERROR_FILTERS = {
     "Signal caught (11)": "RECONNECT",
     "Incorrect challenge, please reconnect": "RECONNECT",
     "Exception Address: 00000005ab99b28c (VMA)": "RECONNECT",
+    "Exception Address: 00000005cb08b28c (VMA)": "RECONNECT",
     "Exception Address: 000000016500b28c (VMA)": "RECONNECT",
     "ERROR: CL_ParseServerMessage: read past end of server message": "RECONNECT",
     "^0^7D^6e^7Frag^6.^7TV^0/^7 was kicked": "RECONNECT"
